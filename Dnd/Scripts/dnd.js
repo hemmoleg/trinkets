91ebf71515c12dff2efb1d873cbf14e24f51a52d
unknown <yakuzaa@gmx.de>
2017-10-22 22:57:02 +0200

    better handling for movement of pieces

--- Dnd/Scripts/dnd.js
+++ Dnd/Scripts/dnd.js
@@ -1,18 +1,35 @@
 var slotsUnordered = [];
 var slots = [];
-var pieces = []; 
+var pieces = [];
 var dragSrcElement = null;
 
-window.onload = function () 
-{   
+function Piece()
+{
+    var Div = "<div class='piece' draggable='true' data-value='2'>2</div>";
+    var x;
+    var y;
+    
+    this.GetX = function(){return x;}
+    this.SetX = function(val){x = val;}
+    
+    this.GetY = function(){return y;}
+    this.SetY = function(val){y = val;}
+    
+    this.GetDiv = function(){return Div;}
+}
+
+window.onload = function ()
+{
+    document.body.addEventListener("keydown", processKey);
+    
     slotsUnordered = document.querySelectorAll('.slot');
-    pieces = document.querySelectorAll('.piece');
     
     var row = [];
     for(var i = 0; i < slotsUnordered.length; i++)
     {
         console.log("slot " + i);
         row[i % 3] = slotsUnordered[i];
+        slotsUnordered[i].innerHTML = "";
     
         if(i % 3 == 2 && i != 0)
         {
@@ -43,9 +60,17 @@ window.onload = function ()
         //.classList.add('over');
     }
     
-    document.body.addEventListener("keydown", processKey); 
-
-    slots[0][1].querySelector(".piece").style.visibility = "hidden";
+    var newPiece = new Piece();
+    pieces[pieces.length] = newPiece;
+    slots[1][0].innerHTML = newPiece.GetDiv();
+    newPiece.SetX(1);
+    newPiece.SetY(0);
+    
+    var newPiece = new Piece();
+    pieces[pieces.length] = newPiece;
+    slots[1][2].innerHTML = newPiece.GetDiv();
+    newPiece.SetX(1);
+    newPiece.SetY(2);
 }
 
 /*
@@ -55,21 +80,125 @@ window.onload = function ()
     d  40
 */
 
+
 function processKey(e)
 {
     console.log(String.fromCharCode(e.keyCode)+" --> "+e.keyCode);
+    
+    for(var i = 0; i < pieces.length; i++)
+    {
+        console.log("Piece: " + i + " x: " + pieces[i].GetX() + " y: " + pieces[i].GetY());   
+    }
+    
+    var pieceMoved = true;
+    
+    // right
     if(e.keyCode == 39)
     {
-        for(i = 0; i < slots.length; i++)
+        console.log("Pieces: " + pieces.length);
+        
+        while(pieceMoved)
         {
-            for(j = 0; j < slots[i].length; j++)
+            pieceMoved = false;
+            for(var i = 0; i < pieces.length; i++)
             {
-                if(slots[i][j+1] != null && slots[i][j+1].querySelector(".piece").style.visibility == "hidden")
+                var piece = pieces[i];
+
+                if(piece.GetY()+1 <= 2 &&
+                   slots[piece.GetX()][piece.GetY()+1].innerHTML == "")
                 {
-                    slots[i][j+1].innerHTML == slots[i][j];
+                    slots[piece.GetX()][piece.GetY()+1].innerHTML = piece.GetDiv();
+                    slots[piece.GetX()][piece.GetY()].innerHTML = "";
+
+                    piece.SetY(piece.GetY() + 1);
+                    pieceMoved = true;
+                    break;
                 }
             }
         }
+    }
+    
+    // left
+    if(e.keyCode == 37)
+    {
+        console.log("Pieces: " + pieces.length);
+        
+        while(pieceMoved)
+        {
+            pieceMoved = false;
+            for(var i = 0; i < pieces.length; i++)
+            {
+                var piece = pieces[i];
+
+                if(piece.GetY()-1 >= 0 &&
+                   slots[piece.GetX()][piece.GetY()-1].innerHTML == "")
+                {
+                    slots[piece.GetX()][piece.GetY()-1].innerHTML = piece.GetDiv();
+                    slots[piece.GetX()][piece.GetY()].innerHTML = "";
+
+                    piece.SetY(piece.GetY() - 1);
+                    pieceMoved = true;
+                    break;
+                }
+            }
+        }
+    }
+    
+    // up
+    if(e.keyCode == 38)
+    {
+        console.log("Pieces: " + pieces.length);
+        
+        while(pieceMoved)
+        {
+            pieceMoved = false;
+            for(var i = 0; i < pieces.length; i++)
+            {
+                var piece = pieces[i];
+
+                if(piece.GetX()-1 >= 0 &&
+                   slots[piece.GetX()-1][piece.GetY()].innerHTML == "")
+                {
+                    slots[piece.GetX()-1][piece.GetY()].innerHTML = piece.GetDiv();
+                    slots[piece.GetX()][piece.GetY()].innerHTML = "";
+
+                    piece.SetX(piece.GetX() - 1);
+                    pieceMoved = true;
+                    break;
+                }
+            }
+        }
+    }
+    
+    // down
+    if(e.keyCode == 40)
+    {
+        console.log("Pieces: " + pieces.length);
+        
+        while(pieceMoved)
+        {
+            pieceMoved = false;
+            for(var i = 0; i < pieces.length; i++)
+            {
+                var piece = pieces[i];
+
+                if(piece.GetX()+1 <= 2 &&
+                   slots[piece.GetX()+1][piece.GetY()].innerHTML == "")
+                {
+                    slots[piece.GetX()+1][piece.GetY()].innerHTML = piece.GetDiv();
+                    slots[piece.GetX()][piece.GetY()].innerHTML = "";
+
+                    piece.SetX(piece.GetX() + 1);
+                    pieceMoved = true;
+                    break;
+                }
+            }
+        }
+    }
+    
+    for(var i = 0; i < pieces.length; i++)
+    {
+        console.log("Piece: " + i + " x: " + pieces[i].GetX() + " y: " + pieces[i].GetY());   
     }
 }
 
