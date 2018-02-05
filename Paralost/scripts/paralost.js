1e4278e7a24e15bca0c6fed040a843910312698c
unknown <rpluntke@cc-college.gecs.loc>
2018-02-05 12:44:48 +0100

    perspective for body / meteors behind skyline1

--- Paralost/scripts/paralost.js
+++ Paralost/scripts/paralost.js
@@ -8,6 +8,9 @@ window.onload = function() {
     meteorMaster = $('.meteor').clone(false);   
     docHeight = document.documentElement.clientHeight;
     docWidth = document.documentElement.clientWidth;
+
+    //$('#skyline1').css('top', '500px');
+    $('#skyline1').css('top', parseInt(docHeight - $('#skyline1').outerHeight()) + 'px');
     
     placeMeteor();
 
@@ -18,7 +21,7 @@ function placeMeteor()
 {
     if(!keepAddingMeteors)
         return;
-    
+   
     meteorHeight = $('.meteor').first().height();
     let newMeteor = $(meteorMaster).clone(false);
     
@@ -27,15 +30,15 @@ function placeMeteor()
     var distance = Math.random() * (max - min) + min;*/
     
     let xPos = Math.floor(Math.random() * (docWidth - 0) + 0);
-    let yPos = Math.floor(Math.random() * (docHeight*0.3 - meteorHeight/2));
+    let yPos = - meteorHeight;
 
-    newMeteor.css('transform', 'skewX(10deg) ' + 'translate('+xPos+'px,'+yPos+'px)');
+    newMeteor.css('transform', 'skewX(10deg) ' + 'translate3d('+xPos+'px,'+yPos+'px, -100px)');
     $('body').append(newMeteor);
 
     console.log('new meteor ' + xPos + ' ' + yPos);
 
-    let targetX = xPos + docHeight * Math.tan(10); // Math.tan(10) = 0.64???
-    TweenMax.to(newMeteor, 5, { x: targetX, y: docHeight, ease: Power1.easeInOut});
+    let targetX = xPos + docHeight * Math.tan(10 * Math.PI/180);
+    TweenMax.to(newMeteor, 5, { x: targetX, y: docHeight, ease: Power1.easeInOut, onComplete: removeMeteor, onCompleteParams:[newMeteor]});
     
     setTimeout(function(){placeMeteor();}, 5000);
 }
@@ -45,4 +48,9 @@ function startStopAddingMeteors()
     keepAddingMeteors = !keepAddingMeteors;
     console.log(keepAddingMeteors);
     placeMeteor();
+}
+
+function removeMeteor(e)
+{
+    $(e).remove();
 }
