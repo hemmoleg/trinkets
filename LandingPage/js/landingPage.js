3594d68c44dbf26f100af0b6996b07e254baebac
r p <yakuzaa@gmx.de>
2018-03-04 23:01:51 +0100

    - transition somewhat done
    Signed-off-by: r p <yakuzaa@gmx.de>

--- LandingPage/js/landingPage.js
+++ LandingPage/js/landingPage.js
@@ -4,6 +4,43 @@ var source;
 var tl;
 var tween;
 
+var TEST_C;
+var target;
+
+function initCurtain()
+{
+    /* a2 + b2 = c2
+            b2 = c2 - a2
+      -c2 + b2 = -a2
+       c2 - b2 = a2
+       height2 - width2 = a2 */
+
+    let a2 = $(window).height() * $(window).height() + ($(window).width() * $(window).width());
+    TEST_C = Math.sqrt(a2);
+    /*$('#curtain').width(TEST_C);
+    $('#curtain').height(TEST_C);
+    $('#curtain').css('top', -TEST_C);
+    $('#curtain').css('left', -TEST_C/2);*/
+    
+    let angle = (360 / (2*Math.PI)) * (Math.atan( $(window).height() / $(window).width() ));
+
+    let scale;
+    if($(window).height() > $(window).width())
+    {
+        scale = TEST_C / ($('#curtain').width());
+    }
+    else
+    {
+        scale = TEST_C / ($('#curtain').height());
+    }
+
+    $('#curtain').css('transform', 'rotate('+ angle +'deg) scale('+scale+')');
+    //$('#curtain').css('transform', 'scale('+scale+')');
+    target = (TEST_C / $(window).height()) * 100 + '%';
+    /*$('#curtain').css('top', '-' + target);
+    $('#curtain').css('left', '-' + target);*/
+}
+
 window.onload = function()
 {
     video = $('#fullscreenBGVideo' )[0];
@@ -11,22 +48,13 @@ window.onload = function()
     
     currentVideo = 1; 
     
-    video.addEventListener('timeupdate', curtainFall);
+    initCurtain();
+
     video.addEventListener('canplaythrough', onCanPlayThrough);
+    video.addEventListener('timeupdate', curtainFall);
     
     currentVideo = 0;
-    //loadNextVideo();
-}
-
-
-function curtainFall()
-{
-    //console.log(video.currentTime);
-    if(tween == null && (video.duration - video.currentTime) <= 1.5)
-    {
-        tween = TweenMax.to($('#curtain'),1.5,{height:"250%", onComplete: loadNextVideo});
-        TweenMax.set($('#curtain'), {height:"10%", top:"-40%", left:"-55%"})
-    }
+    loadNextVideo();
 }
 
 function loadNextVideo()
@@ -41,8 +69,19 @@ function loadNextVideo()
 
 function onCanPlayThrough()
 {
+    //return;
     video.play();
     //video.pause();
-    TweenMax.to($('#curtain'),1.5,{height:"10%", top:"200%", left:"80%"});
+    TweenMax.to($('#curtain'),1.5,{top: target, left: target});
     tween = null;
+}
+
+function curtainFall()
+{
+    //console.log(video.currentTime);
+    if(tween == null && (video.duration - video.currentTime) <= 1.5)
+    {
+        tween = TweenMax.to($('#curtain'),1.5,{top:"0%", left:"0%", onComplete: loadNextVideo});
+        TweenMax.set($('#curtain'), {top:"-" + target, left:"-" + target})
+    }
 }
