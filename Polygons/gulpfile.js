15a337061b752326a4acdf904106955c27e6fa52
r p <yakuzaa@gmx.de>
2018-05-03 17:02:15 +0200

    - automation - api call - azure function call

--- Polygons/gulpfile.js
+++ Polygons/gulpfile.js
@@ -1,6 +1,7 @@
 // Less configuration
 var gulp = require('gulp');
-var less = require('gulp-less');
+var less = require('gulp-less')
+var tsc = require('gulp-typescript')
 
 gulp.task('less', function() {
     gulp.src('css/*.less')
@@ -12,6 +13,20 @@ gulp.task('less', function() {
         }))
 });
 
-gulp.task('default', ['less'], function() {
+gulp.task('watch_less', ['less'], function() {
     gulp.watch('css/*.less', ['less']);
-})
+});
+
+gulp.task("typescript", function () {
+    return gulp.src('js/*.ts')
+        .pipe(tsc({
+            noImplicitAny: true,
+        }))
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }))
+});
+
+gulp.task('watch_typescript', ['typescript'], function(){
+    gulp.watch('js/*.ts', ['typescript']);
+});
